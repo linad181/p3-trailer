781e5e8037f74d2b2aefaa4b63f0722f185b0317
Pecka <fpecka@engr-134-21.eastern.ewu.edu>
2015-05-14 09:39:59 -0700

    Added stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 // My Scripts
 
-document.write("Hello World!");
+// Media Element Player
+
+$('audio,video').mediaelementplayer({
+	success: function(player, node) {
+	  $('#' + node.id + '-mode').html('mode: ' + player.pluginType);
+	},
+	startLanguage: 'en',
+	translationSelector: true
+});
